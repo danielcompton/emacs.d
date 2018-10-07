97c656f09207599b0a796b0ab14fd5aa49d42f9b
danielcompton <desk@danielcompton.net>
2018-10-07 22:33:52 +1300

    Use stable melpa

--- core/dc-packages.el
+++ core/dc-packages.el
@@ -2,8 +2,17 @@
 (require 'cl)
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+
+;; https://emacs.stackexchange.com/a/2989
+(setq package-archives
+      '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
+        ("MELPA Stable" . "https://stable.melpa.org/packages/")
+        ("MELPA"        . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("MELPA Stable" . 10)
+        ("GNU ELPA"     . 5)
+        ("MELPA"        . 0)))
+
 (package-initialize)
 
 ;; Bootstrap `use-package`
@@ -17,5 +26,17 @@
 (use-package magit
   :bind (("C-x g" . magit-status)
 	 ("C-x M-g" . magit-dispatch-popup)))
+
+(use-package projectile
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (setq projectile-cache-file (expand-file-name  "projectile.cache" prelude-savefile-dir))
+  (projectile-mode 1))
+
+
+
+
+
 
 (provide 'dc-packages)
