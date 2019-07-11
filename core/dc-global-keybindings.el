2e170a0cc6c96f9481b1e0ead1e9db27ce13d230
Daniel Compton <desk@danielcompton.net>
2019-07-11 14:29:26 +1200

    Add some global keybindings

--- core/dc-global-keybindings.el
+++ core/dc-global-keybindings.el
@@ -1,4 +1,19 @@
 
+;; Use ESC as universal get me out of here command
+(define-key key-translation-map (kbd "ESC") (kbd "C-g"))
 
+;; Things you'd expect from macOS app.
+(global-set-key (kbd "s-s") 'save-buffer)             ;; save
+(global-set-key (kbd "s-S") 'write-file)              ;; save as
+(global-set-key (kbd "s-q") 'save-buffers-kill-emacs) ;; quit
+(global-set-key (kbd "s-a") 'mark-whole-buffer)       ;; select all
+(global-set-key (kbd "s-z") 'undo) ;; undo
+(global-set-key (kbc "s-v") 'yank) ;; paste
+(global-set-key (kbd "s-x") 'kill-region) ;; cut
+(global-set-key (kbc "s-c") 'copy-region-as-kill) ;; copy
+
+
+;; Comment line or region.
+(global-set-key (kbd "s-/") 'comment-line)
 
 (provide 'dc-global-keybindings)
